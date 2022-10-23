1abf01ad74e00a4cf55fc38dd02854da014ae5dc
QuangQuyen <quangquyendad@gmail.com>
2022-10-23 14:52:52 +0700

    update;

--- [BÀI TẬP] APP_FOOD.sql
+++ [BÀI TẬP] APP_FOOD.sql
@@ -1,106 +1,73 @@
--- -------------------------------------------------------------
--- TablePlus 5.0.0(454)
---
--- https://tableplus.com/
---
--- Database: app_food
--- Generation Time: 2022-10-23 10:32:44.2780
--- -------------------------------------------------------------
+
+CREATE TABLE order(
+	user_id INT,
+	food_id INT,
+	amount INT,
+	order_code VARCHAR(255),
+	arr_sub_id VARCHAR(255),
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (food_id) REFERENCES food(food_id)
+)
+
+-- 23 October 2022 10:23:59 AM
+CREATE TABLE sub_food (
+	sub_id INT PRIMARY KEY AUTO_INCREMENT,
+	sub_name VARCHAR(255),
+	sub_price FLOAT,
+	food_id INT ,
+	FOREIGN KEY (food_id) REFERENCES food(food_id)
+)
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- 23 October 2022 10:23:01 AM
+CREATE TABLE food(
+	food_id INT PRIMARY KEY AUTO_INCREMENT,
+	food_name VARCHAR(255),
+	image VARCHAR(255),
+	price FLOAT,
+	description VARCHAR(255),
+	type_id INT,
+	FOREIGN KEY (type_id) REFERENCES food_type(type_id)
+)
+
+-- 23 October 2022 10:21:07 AM
+CREATE TABLE food_type (
+	type_id INT PRIMARY KEY AUTO_INCREMENT,
+	type_name VARCHAR(255)
+)
+
+-- 23 October 2022 10:17:38 AM
+CREATE TABLE like_res(
+	user_id INT ,
+	res_id INT,
+	date_like DATETIME,
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
+)
+
+-- 23 October 2022 10:14:57 AM
+CREATE TABLE rate_res(
+	user_id INT,
+	res_id INT,
+	amount INT,
+	date_rate DATETIME,
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
+)
 
 
-CREATE TABLE `food` (
-  `food_id` int NOT NULL AUTO_INCREMENT,
-  `food_name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `type_id` int DEFAULT NULL,
-  PRIMARY KEY (`food_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `food_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `food_type` (`type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `food_order` (
-  `user_id` int DEFAULT NULL,
-  `food_id` int DEFAULT NULL,
-  `amount` int DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `arr_sub_id` varchar(255) DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `food_id` (`food_id`),
-  CONSTRAINT `food_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `food_order_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `food_type` (
-  `type_id` int NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `like_res` (
-  `user_id` int DEFAULT NULL,
-  `res_id` int DEFAULT NULL,
-  `date_like` datetime DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `res_id` (`res_id`),
-  CONSTRAINT `like_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `like_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `rate_res` (
-  `user_id` int DEFAULT NULL,
-  `res_id` int DEFAULT NULL,
-  `amount` int DEFAULT NULL,
-  `date_rate` datetime DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `res_id` (`res_id`),
-  CONSTRAINT `rate_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `rate_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `restaurant` (
-  `res_id` int NOT NULL AUTO_INCREMENT,
-  `res_name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`res_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `sub_food` (
-  `sub_id` int NOT NULL AUTO_INCREMENT,
-  `sub_name` varchar(255) DEFAULT NULL,
-  `sub_price` float DEFAULT NULL,
-  `food_id` int DEFAULT NULL,
-  PRIMARY KEY (`sub_id`),
-  KEY `food_id` (`food_id`),
-  CONSTRAINT `sub_food_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- 23 October 2022 10:01:47 AM
+CREATE TABLE restaurant(
+	res_id INT PRIMARY KEY AUTO_INCREMENT,
+	res_name VARCHAR(255),
+	image VARCHAR(255),
+	description VARCHAR(255),
 
 
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- 23 October 2022 9:58:27 AM
+CREATE TABLE user(
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+	full_name VARCHAR(255),
+	email VARCHAR(255),
+	password VARCHAR(255)
+)
